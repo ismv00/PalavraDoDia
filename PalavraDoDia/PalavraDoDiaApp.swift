8d64e4ea659fd574a406b6995ab386d38cc832b3
ismv00 <ismv00@icloud.com>
2024-11-01 22:51:30 -0400

    Initial Commit

---
+++ PalavraDoDia/PalavraDoDiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalavraDoDiaApp.swift
+//  PalavraDoDia
+//
+//  Created by Igor S. Menezes on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PalavraDoDiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
